c2396e78c95100d43ef0668be83ea538836a2eb6
Gabriel Cuin dos Santos <gabrielcuinprof@gmail.com>
2022-03-27 16:11:21 -0300

    Projeto pronto para publicar

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,35 @@
-import React from "react";
-import Link from "../src/components/Link/index.js";
+import HomeScreen from "../src/screens/HomeScreen";
+
+export default HomeScreen;
+
+// import React from "react";
+// import Link from "../src/components/Link/index.js";
+// import Footer from "../src/components/Link/patterns/Footer/index.js";
 
 
+// function Title({ children, as }) {
+//   const Tag = as;
 
-function Title({ children, as }) {
-  const Tag = as;
+//   return (
+//     <React.Fragment>
+//       <Tag>{children}</Tag>
+//       <style jsx>
+//         {`
+//           ${Tag} {
+//             color: red;
+//           }
+//         `}
+//       </style>
+//     </React.Fragment>
+//   );
+// }
 
-  return (
-    <React.Fragment>
-      <Tag>{children}</Tag>
-      <style jsx>
-        {`
-          ${Tag} {
-            color: red;
-          }
-        `}
-      </style>
-    </React.Fragment>
-  );
-}
-
-export default function HomePage() {
-  return (
-    <div>
-      <Title as="h2">Alura Cases - Home Page</Title>
-      <Link href="/faq">Ir para a página do FAQ</Link>
-    </div>
-  );
-}
+// export default function HomePage() {
+//   return (
+//     <div>
+//       <Title as="h2">Alura Cases - Home Page</Title>
+//       <Link href="/faq">Ir para a página do FAQ</Link>
+//       <Footer/>
+//     </div>
+//   );
+// }
